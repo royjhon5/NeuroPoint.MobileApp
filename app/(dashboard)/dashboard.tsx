5a8a4c77e2bf194181124e0dfc1e3ca5ceb1a61b
DESKTOP-VLTCHR6\Roy Developer <rjisraelcubillan55@gmail.com>
2025-08-08 16:13:45 +0800

    update: latest update

--- app/(dashboard)/dashboard.tsx
+++ app/(dashboard)/dashboard.tsx
@@ -1,31 +1,12 @@
-import WhyComponent from "@/app/sections/WhySection";
 import { useAuth } from "@/context/AuthContext";
 import { useRouter } from "expo-router";
-import { useEffect, useMemo, useState } from "react";
-import { useWindowDimensions, View } from "react-native";
+import { useEffect } from "react";
+import { Text, View } from "react-native";
 import { ActivityIndicator } from "react-native-paper";
-import { SceneMap, TabView } from "react-native-tab-view";
-
-const renderScene = SceneMap({
-  first: WhyComponent,
-  second: WhyComponent,
-  third: WhyComponent,
-});
 
 export default function Dashboard() {
-  const layout = useWindowDimensions();
-  const [index, setIndex] = useState<number>(0);
   const { isAuthenticated, isAuthLoading } = useAuth();
   const router = useRouter();
-
-  const routes = useMemo(
-    () => [
-      { key: "first", title: "My Courses" },
-      { key: "second", title: "Library" },
-      { key: "third", title: "Leaderboard" },
-    ],
-    []
-  );
 
   useEffect(() => {
     if (!isAuthLoading && !isAuthenticated) {
@@ -36,19 +17,16 @@ export default function Dashboard() {
   if (isAuthLoading) {
     return (
       <View className="p-5 flex items-center justify-center">
-        <ActivityIndicator animating={true} />
+        <ActivityIndicator animating={isAuthLoading} />
       </View>
     );
   }
 
   return (
     <>
-      <TabView
-        navigationState={{ index, routes }}
-        renderScene={renderScene}
-        onIndexChange={setIndex}
-        initialLayout={{ width: layout.width || 360 }}
-      />
+      <View>
+        <Text>Hello World</Text>
+      </View>
     </>
   );
 }
