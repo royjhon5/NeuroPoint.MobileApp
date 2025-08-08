27274aaf5401f452c457c24ea8a97a7ba0a55dea
DESKTOP-VLTCHR6\Roy Developer <rjisraelcubillan55@gmail.com>
2025-08-08 16:32:00 +0800

    update: latest update

--- app/(dashboard)/dashboard.tsx
+++ app/(dashboard)/dashboard.tsx
@@ -1,12 +1,25 @@
+import WhyComponent from "@/app/sections/WhySection";
 import { useAuth } from "@/context/AuthContext";
 import { useRouter } from "expo-router";
-import { useEffect } from "react";
-import { Text, View } from "react-native";
+import { useEffect, useMemo, useState } from "react";
+import { useWindowDimensions, View } from "react-native";
 import { ActivityIndicator } from "react-native-paper";
+import { TabView } from "react-native-tab-view";
 
 export default function Dashboard() {
+  const layout = useWindowDimensions();
+  const [index, setIndex] = useState<number>(0);
   const { isAuthenticated, isAuthLoading } = useAuth();
   const router = useRouter();
+
+  const routes = useMemo(
+    () => [
+      { key: "first", title: "My Courses" },
+      { key: "second", title: "Library" },
+      { key: "third", title: "Leaderboard" },
+    ],
+    []
+  );
 
   useEffect(() => {
     if (!isAuthLoading && !isAuthenticated) {
@@ -17,16 +30,31 @@ export default function Dashboard() {
   if (isAuthLoading) {
     return (
       <View className="p-5 flex items-center justify-center">
-        <ActivityIndicator animating={isAuthLoading} />
+        <ActivityIndicator animating={true} />
       </View>
     );
   }
 
+  const renderScene = ({ route }: { route: { key: string } }) => {
+    switch (route.key) {
+      case "first":
+        return <WhyComponent />;
+      case "second":
+        return <WhyComponent />;
+      case "third":
+        return <WhyComponent />;
+      default:
+        return null;
+    }
+  };
+
   return (
-    <>
-      <View>
-        <Text>Hello World</Text>
-      </View>
-    </>
+    <TabView
+      navigationState={{ index, routes }}
+      renderScene={renderScene}
+      lazy
+      onIndexChange={setIndex}
+      initialLayout={{ width: layout.width || 360 }}
+    />
   );
 }
